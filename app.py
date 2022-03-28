6695b4d7b32922a054becc2aa0ac4b16ede1848e
Viridia Emmaline <viridiaemmaline@gmail.com>
2022-03-28 15:53:23 -0400

    big upload lol

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask# , render_template, g, request
+# from datetime import datetime
+# from flask_babel import Babel, format_datetime
+
+app = Flask(__name__)
+# app.config.from_pyfile('mysettings.cfg')
+# babel = Babel(app)
+
+# @babel.localeselector
+# def get_locale():
+#     # if a user is logged in, use the locale from the user settings
+#     user = getattr(g, 'user', None)
+#     if user is not None:
+#         return user.locale
+#     # otherwise try to guess the language from the user accept
+#     # header the browser transmits.  We support de/fr/en in this
+#     # example.  The best match wins.
+#     return request.accept_languages.best_match(['de', 'fr', 'en'])
+
+# @babel.timezoneselector
+# def get_timezone():
+#     user = getattr(g, 'user', None)
+#     if user is not None:
+#         return user.timezone
+    
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+# @app.route("/hello/")
+# @app.route("/hello/<name>")
+# def hello_there(name = None):
+#     return render_template(
+#         "hello_there.html",
+#         name=name,
+#         date=format_datetime("EEEE, dd MMMM, yyyy at HH:mm:ss.")
+#     )
+
