064f3b54dbb1a4b92c9d1102842f444d76eb44b0
Viridia Emmaline <viridiaemmaline@gmail.com>
2022-03-28 18:39:35 -0400

    added a lot lol

--- app.py
+++ app.py
@@ -1,38 +1,37 @@
-from flask import Flask# , render_template, g, request
-# from datetime import datetime
-# from flask_babel import Babel, format_datetime
+from flask import Flask, render_template, g, request
+from datetime import datetime
+from flask_babel import Babel, format_datetime
 
 app = Flask(__name__)
-# app.config.from_pyfile('mysettings.cfg')
-# babel = Babel(app)
+babel = Babel(app)
 
-# @babel.localeselector
-# def get_locale():
-#     # if a user is logged in, use the locale from the user settings
-#     user = getattr(g, 'user', None)
-#     if user is not None:
-#         return user.locale
-#     # otherwise try to guess the language from the user accept
-#     # header the browser transmits.  We support de/fr/en in this
-#     # example.  The best match wins.
-#     return request.accept_languages.best_match(['de', 'fr', 'en'])
+@babel.localeselector
+def get_locale():
+    # if a user is logged in, use the locale from the user settings
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    # otherwise try to guess the language from the user accept
+    # header the browser transmits.  We support de/fr/en in this
+    # example.  The best match wins.
+    return request.accept_languages.best_match(['de', 'fr', 'en'])
 
-# @babel.timezoneselector
-# def get_timezone():
-#     user = getattr(g, 'user', None)
-#     if user is not None:
-#         return user.timezone
+@babel.timezoneselector
+def get_timezone():
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.timezone
     
 @app.route("/")
 def home():
     return "Hello, Flask!"
 
-# @app.route("/hello/")
-# @app.route("/hello/<name>")
-# def hello_there(name = None):
-#     return render_template(
-#         "hello_there.html",
-#         name=name,
-#         date=format_datetime("EEEE, dd MMMM, yyyy at HH:mm:ss.")
-#     )
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=format_datetime(datetime.now(), "EEEE, MMMM dd, yyyy 'at' h:m.")
+    )
 
---
+++ consthandler.py
@@ -0,0 +1,30 @@
+import csv
+import random
+
+def getcsv(filename:str = "aspects.csv"):
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return {item['base']:item for item in list(reader)}
+    
+def getClasspect(classpects: list, duals:bool = False):
+    rolls = []
+    if duals:
+        for i in classpects.values(): rolls.append(i[random.choice(list(i.keys()))][random.choice(list(i.keys()))])
+    else: 
+        for i in classpects.values(): rolls.append(random.choice(list(i.keys())))
+    return rolls
+    
+def main():
+    classpects = {"classes": getcsv("classes.csv"), "aspects": getcsv("aspects.csv")}
+
+    print("Normals:")
+    for i in range(12):
+        roll = getClasspect(classpects)
+        print(roll[0],"of",roll[1])
+        
+    print("\nDuals:")
+    for i in range(12):
+        roll = getClasspect(classpects, duals=True)
+        print(roll[0],"of",roll[1])
+            
+main()
